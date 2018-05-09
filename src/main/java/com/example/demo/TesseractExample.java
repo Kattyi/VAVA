348ea32d2fe83dfad4a466af871bbb6b3ed6b187
Katarina Rafcikova <rafcikova@gmail.com>
2018-05-09 19:16:19 +0200

    Added logs to file, showing converted text

--- src/main/java/com/example/demo/TesseractExample.java
+++ src/main/java/com/example/demo/TesseractExample.java
@@ -5,27 +5,45 @@ import net.sourceforge.tess4j.*;
 
 public class TesseractExample {
 
-    public static void main(String[] args) {
+    public String converter(String path) {
 
-        System.out.println("hello");
+        File imageFile = new File(path);
 
-        File imageFile = new File("/Users/KatCake/Desktop/test.png");
-        /**
-         * JNA Interface Mapping
-         **/
         ITesseract instance = new Tesseract();
 
-        /**
-         * You either set your own tessdata folder with your custom language pack or
-         * use LoadLibs to load the default tessdata folder for you.
-         **/
         instance.setDatapath(".");
 
         try {
             String result = instance.doOCR(imageFile);
-            System.out.println(result);
+            return result;
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
+
+        return null;
     }
+
+//    public static void main(String[] args) {
+//
+//        System.out.println("hello");
+//
+//        File imageFile = new File("/Users/KatCake/Desktop/demo/upload-dir/test.png");
+//        /**
+//         * JNA Interface Mapping
+//         **/
+//        ITesseract instance = new Tesseract();
+//
+//        /**
+//         * You either set your own tessdata folder with your custom language pack or
+//         * use LoadLibs to load the default tessdata folder for you.
+//         **/
+//        instance.setDatapath(".");
+//
+//        try {
+//            String result = instance.doOCR(imageFile);
+//            System.out.println(result);
+//        } catch (TesseractException e) {
+//            System.err.println(e.getMessage());
+//        }
+//    }
 }
